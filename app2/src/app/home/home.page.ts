b44d9d0a5d7e2955fff10ab2905b0fbc1e074e8b
SilviaDumitrache <silvia_d97@yahoo.com>
2022-06-03 12:20:36 +0300

    create new account, upload profile pic to Firebase

--- app2/src/app/home/home.page.ts
+++ app2/src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { ProfilepicService } from '../services/profilepic.service';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { LoadingController, AlertController } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,17 @@ export class HomePage {
   profile = null;
 
   constructor(
-    private profilePic: ProfilepicService,
+    private profilepicService: ProfilepicService,
     private authService: AuthService,
     private router: Router,
     private loadingController: LoadingController,
     private alertController: AlertController
 
-  ) {}
+  ) {
+    this.profilepicService.getUserProfile().subscribe((data) => {
+      this.profile = data;
+    });
+  }
 
   //logout function
   async logout(){
@@ -27,8 +32,32 @@ export class HomePage {
     this.router.navigateByUrl('/', {replaceUrl: true});
   }
 
-  async changePic(){
+  async changeImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Photos, //set to photos, but can be changed to Camera/Prompt
+    });
+    console.log(image);
 
+    if (image) {
+      const loading = await this.loadingController.create();
+      await loading.present();
+
+      const result = await this.profilepicService.uploadImage(image);
+      loading.dismiss();
+
+      if (!result) {
+        const alert = await this.alertController.create({
+          header: 'Incarcarea imaginii a esuat',
+          message: 'A aparut o problema in timpul incarcarii imaginii.',
+          buttons: ['OK'],
+        });
+        await alert.present();
+
+      }
+    }
   }
 
 }
